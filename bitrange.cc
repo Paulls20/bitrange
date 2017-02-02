1e10273db2522e608883c940c1356c7f55ec3d46
Paul Varghese <paullls20@gmail.com>
2017-02-03 02:19:37 +0530

    Create bitrange.cc

---
+++ bitrange.cc
@@ -0,0 +1,24 @@
+#include <bitset>
+
+// the range [R, L) == [R, L - 1]
+// return the result at compile time
+template<std::size_t R, std::size_t L, std::size_t N>
+constexpr void set_range(std::bitset<N>& bits)
+{
+    static_assert(R <= L && L <= N, "invalid range");
+    std::size_t numOnes = L - R; 
+    std::bitset<N> mask;
+    mask.set();
+    mask <<= numOnes; 
+    mask.flip();
+    mask <<= R ;
+    bits |= mask;
+}
+
+// test set_range
+int main()
+{
+    std::bitset<4> bar;
+    set_range<0,4>(bar);
+    return bar.to_ulong();
+}
